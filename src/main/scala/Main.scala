42a8bfe9ca52b2b87a987126cd9b86771344a071
IxTHING <jordan.white191191@yahoo.co.uk>
2018-01-13 14:54:27 +0000

    Exercise Continuation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,21 @@
 object Main {
-  def main(args: Array[String]) = {println("Hello World!")}
+
+  var helloWorld = "Hello World"
+
+  def returnString(input: String): String = input
+
+  def returnAll(input: Any): Any = input
+
+  def returnPartOfString(stringInput: String, intInput: Int): String = stringInput.substring(intInput,stringInput.length())
+
+  def returnFullWord(s1: String, s2: String, s3: Char, s4: Char): String = s1.concat(s2).replace(s3,s4)
+
+  def returnValue(num1: Int, num2: Int, b: Boolean): Int = if(b){num1 + num2} else {num1 * num2}
+
+  var r = "";
+  def printMultipleLines(word: String, value: Int) = for(i <- 1 to value) println(word)
+
+  def main(args: Array[String])  = {}
 }
+
+
