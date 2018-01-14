0d5f154fbc70927713dadc821898921e44706245
IxTHING <jordan.white191191@yahoo.co.uk>
2018-01-14 12:56:41 +0000

    Exercise Continuation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import java.util.TimeZone
+
 object Main {
 
   var helloWorld = "Hello World"
@@ -12,10 +14,46 @@ object Main {
 
   def returnValue(num1: Int, num2: Int, b: Boolean): Int = if(b){num1 + num2} else {num1 * num2}
 
-  var r = "";
+  def returnValuePatternMatch (num1: Int, num2: Int, b: Boolean): Int = b match {
+    case true => num1 + num2
+    case false => num1 * num2
+  }
+
   def printMultipleLines(word: String, value: Int) = for(i <- 1 to value) println(word)
 
-  def main(args: Array[String])  = {}
+  def printSquare(word: String, value: Int) = for(i <- 1 to value){for(i <- 1 to value){print(word);}
+    println();}
+
+  def inputSwap(input: AnyRef): Any = input match {
+    case  input:List[Any] =>  input.reverse
+    case input:Seq[Any] => input.reverse
+    case input:Array[input] => input.reverse.toString();
+    case input:(Any,Any) => input.swap
+    case _ => "Nothing to return"
+  }
+
+  def printTimezone(location: String): Unit = {
+    val id = location + "/"
+    val a = TimeZone.getAvailableIDs.filter(_.startsWith(id)).map(_.stripPrefix(id)).sorted
+    a.foreach(println)
+  }
+
+  val numbers = Array(1, 2)
+  def main(args: Array[String])  = {printTimezone("Africa")}
+}
+
+object SquarePrint {
+  var x = 0;
+  var y = 0;
+  def characterPrint(c: Char, n: Int): Unit = {
+    if (x < n) {
+      print(c); x += 1
+    } else {
+      println(); x = 0
+      y+=1;
+    }
+    if (y < n) {characterPrint(c, n)}
+  }
 }
 
 
